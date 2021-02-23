cbf208d59b4bbf44146db7b7bf0c5fe6c4e5a9c1
Evan Sayles <evan.sayles@gmail.com>
2021-02-23 13:08:06 -0500

    Initial commit

---
+++ src/LLQueue.java
@@ -0,0 +1,13 @@
+import java.util.LinkedList;
+
+public class LLQueue<E> {
+    public LLQueue(){}
+
+    public boolean isEmpty(){return false;}
+
+    public void enqueue(E object){}
+
+    public E dequeue(){return null;}
+
+    public E peekFront(){return null;}
+}
---
+++ src/LLStack.java
@@ -0,0 +1,25 @@
+import java.util.LinkedList;
+
+public class LLStack<E> {
+    public LLStack(){}
+
+    public boolean isEmpty()
+    {
+        return false;
+    }
+
+    public void push(E object)
+    {
+
+    }
+
+    public E pop()
+    {
+        return null;
+    }
+
+    public E peekTop()
+    {
+        return null;
+    }
+}
